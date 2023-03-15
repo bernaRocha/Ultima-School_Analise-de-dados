22ab70815454c1544b25a6f8c4505ae03a7e4acc
Bernardo <mrochabernardo@gmail.com>
2023-03-15 17:42:33 -0300

    .

--- modulo04-semana4/frame_clause.sql
+++
@@ -1,7 +0,0 @@
-SELECT CustomerId,
-      UnitPrice,
-      AVG(UnitPrice) OVER (PARTITION BY CustomerId
-      ORDER BY CustomerId
-      ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS “CumAvg”
- FROM Orders 
- INNER JOIN [Order Details] ON Orders.OrderId = [Order Details].OrderID 
